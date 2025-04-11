d60317453087c2f894a198d61e6cd65db9b0fa78
demei1 <devinmei6@gmail.com>
2025-04-10 22:29:50 -0700

    Update OnlinePurchaseManager.java

--- src/main/java/OnlinePurchaseManager.java
+++ src/main/java/OnlinePurchaseManager.java
@@ -27,11 +27,9 @@ public class OnlinePurchaseManager
   public boolean hasAdjacentEqualPair()
   {
     /* to be implemented in part (b) */
-    for (int i = 0 ; i < purchases.size()-1;i++){
-      if ( purchases.get(i).equals(purchases.get(i+1)){
+    for (int i = 0 ; i < purchases.size()-1;i++)
+      if ( purchases.get(i).equals(purchases.get(i+1))
         return true;
-      }
-    }
       return false;
   }
   public OnlinePurchaseManager()
